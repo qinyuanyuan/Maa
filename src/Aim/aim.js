c936d8d63df4cb0c31e4b9a5066e2747605fcdc3
LiDanYang0515 <1573146127@qq.com>
2019-12-03 11:34:26 +0800

    myaim\

--- src/Aim/aim.js
+++ src/Aim/aim.js
@@ -1,23 +1,26 @@
 import React from 'react';
-import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom';
+import { Input } from 'antd';
 import logo from './../Logo/logo';
 var AimCss = require('./aim.css');
 
-
+const { TextArea } = Input;
 export default class AimPage extends React.Component{
+  
     render(){
         return(
             <div className={AimCss.aim}> 
                 <div className={AimCss.header}>
-                   <Link to="/homepage" className={AimCss.png}>
-                        <img src={require("./../Img/rrrrr.png")}/>
+                   <Link to="/homepage" className={AimCss.comeback}>
+                   <img src={require("./../Img/13.png")} height="50px" width="50px" />
                    </Link>
-                <img src={require("./../Img/aim1.png")} className={AimCss.img}/>
+                
                 <h1>我的小目标</h1>
                  </div>
               <div className={AimCss.xin} >
-                  <img src={require("./../Img/6d3e330d2de899ec64dc0729d7b458aa1.png" )}/>
+                  
                    <h2>行程</h2>
+                   <TextArea className={AimCss.text1} rows={4} />
                    <div className={AimCss.nian} >
                         <input type="text" size="4"/>
                         <h4>年</h4>
@@ -34,17 +37,16 @@ export default class AimPage extends React.Component{
                         <input type="text" size="4"/>
                         <h4>时</h4>
                     </div>
-                   <input type="text" className={AimCss.text1}></input>
+                  
               </div>
               <div className={AimCss.richeng} >
-                   <img src={require("./../Img/ric1.png")}/>
+                   
                    <h2>日程</h2>
-                   <input type="text" className={AimCss.div1}></input>
+                   <TextArea className={AimCss.text2} rows={4} />
               </div> 
-              <div className={AimCss.footer} >
-                 <img src={require("./../Img/foot1.png")}/>
-              </div>    
+           
         </div>
         )
     }
 }
+// ReactDOM.render(<TextArea rows={4} />, mountNode);
